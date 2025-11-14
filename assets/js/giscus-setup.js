801e81d8f6fca06598f0065f8073d5b483f901e1
Wastedzz <45916523+Wastedzz@users.noreply.github.com>
2025-11-14 12:36:02 +0000

    Deploying to gh-pages from @ Wastedzz/Wastedzz.github.io@fd10ef30722195b6126da01636fa87447a36c485 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Wastedzz/chenguangwang.github.io",
+    "data-repo": "Wastedzz/Wastedzz.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
